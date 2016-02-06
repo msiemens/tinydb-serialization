468b4567c03b62ff0038346b3a1049e415b776c8
ASMfreaK <cpp.create@gmail.com>
2016-02-06 16:12:51 +0300

    Add recursive test

--- test_serialization.py
+++ test_serialization.py
@@ -42,3 +42,21 @@ def test_serializer_nondestructive(tmpdir):
     data_before = dict(data)  # implicitly copy
     db.insert(data)
     assert data == data_before
+
+def test_serializer_recursive(tmpdir):
+    path = str(tmpdir.join('db.json'))
+
+    serializer = SerializationMiddleware(JSONStorage)
+    serializer.register_serializer(DateTimeSerializer(), 'TinyDate')
+    db = TinyDB(path, storage=serializer)
+
+    date = datetime(2000, 1, 1, 12, 0, 0)
+    datenow = datetime.utcnow()
+    dates = [{"date":date,"hp":100}, {"date":datenow,"hp":1}]
+    data = {"dates":dates, "int":10}
+    db.insert(data)
+    db.insert({'int': 2})
+    assert db.count(where('dates') == date) == 1
+    assert db.count(where('int') == 2) == 1
+    assert db.get(where('int')==10).["dates"] == dates
+    
